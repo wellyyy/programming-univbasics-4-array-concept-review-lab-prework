fe366b8d1bc101e3e434eb87941680c64e8b04df
Wesley Drake Faircloth <wesfairclothh@gmail.com>
2020-07-22 23:03:02 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,33 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
-end
-
+   num = array.length
+   num.times do |index|
+     if array[index] == value_to_find
+       return index
+     end
+   end
+   return nil
+ end
 def find_max_value(array)
-  # Add your solution here
+  bigboy = array[0]
+  num = array.length
+  
+  num.times do |index|
+    if bigboy < array[index]
+      bigboy = array[index]
+    end
+    
+  end
+    bigboy
 end
 
 def find_min_value(array)
-  # Add your solution here
+  smallboy= array[0]
+  num = array.length
+  num.times do |index|
+    if smallboy > array[index]
+      smallboy = array[index]
+    end
+  end
+  smallboy
 end
+
